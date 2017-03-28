3b65f415d09d66470dbb409a8f20b2ae28548e3a
Charles Barto <bartoc@umich.edu>
2017-03-28 18:27:10 -0400

    Added CGAL

--- CGAL/conanfile.py
+++ CGAL/conanfile.py
@@ -9,8 +9,9 @@ class CGALConanFile(ConanFile):
     license = "GPLv3"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False],
-               "header_only": [True, False]}
-    default_options = "shared=False", "header_only=False"
+               "header_only": [True, False],
+               "enable_gmp": [True, False]}
+    default_options = "shared=False", "header_only=False", "enable_gmp=False"
     requires = (
         ("Boost/1.64.0b1@barcharcraz/testing")
     )
@@ -21,14 +22,18 @@ class CGALConanFile(ConanFile):
         zip_name = f"{self.name}-{self.version}.zip"
         download_uri = f"https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-{self.version}/CGAL-{self.version}.zip"
         tools.download(download_uri, zip_name)
+        tools.unzip(zip_name)
         shutil.move(f"{self.name}-{self.version}", f"{self.name}")
+        tools.replace_in_file("CGAL/CMakeLists.txt", "CMAKE_SOURCE_DIR", "PROJECT_SOURCE_DIR")
         os.unlink(zip_name)
 
     def build(self):
         cmake = CMake(self.settings)
         cmake_options = [
             f"-DCMAKE_INSTALL_PREFIX={self.package_folder}",
-            f"-DBUILD_SHARED_LIBS={self.options.shared}"
+            f"-DBUILD_SHARED_LIBS={self.options.shared}",
+            f"-DCGAL_HEADER_ONLY={self.options.header_only}",
+            f"-DCGAL_DISABLE_GMP={not self.options.enable_gmp}"
         ]
         self.run(f"cmake . {cmake.command_line} {' '.join(cmake_options)}")
         self.run(f"cmake --build . --target install {cmake.build_config}")
