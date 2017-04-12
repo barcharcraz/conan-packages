694501d388c834a4c7427f947277ef568ce37ee8
bartoc <bartoc@umich.edu>
2017-04-12 12:53:21 -0400

    added libigl

---
+++ libigl/conanfile.py
@@ -0,0 +1,28 @@
+import conans
+from conans import tools, ConanFile, CMake
+from conans.tools import download, unzip
+import os
+import shutil
+
+class LibiglConan(ConanFile):
+    name = "libigl"
+    version = "2bc57eb"
+    description = "libigl geometry library"
+    author = "Charles Barto"
+    license = "MPL"
+    settings = ()
+    options = {}
+    exports = "CMakeLists.txt"
+    generators = "cmake"
+    def source(self):
+        self.run("git clone https://github.com/libigl/libigl.git")
+        self.run(f"git -C libigl checkout {self.version}")
+    def build(self):
+        cmake = CMake(self.settings)
+        args = [f"-DCMAKE_INSTALL_PREFIX={self.package_folder}"
+                ]
+        self.run(f"cmake . {cmake.command_line} {' '.join(args)}")
+        self.run(f"cmake --build . {cmake.build_config}")
+    def package(self):
+        cmake = CMake(self.settings)
+        self.run(f"cmake --build . --target install {cmake.build_config}")
