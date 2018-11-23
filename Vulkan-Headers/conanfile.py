8906fd39eb6edfeeaf5bb98de4ffada5be077ce1
Charles Barto <bartoc@umich.edu>
2018-11-22 19:44:13 -0500

    add in vulkan packages

--- Vulkan-Headers/conanfile.py
+++ Vulkan-Headers/conanfile.py
@@ -1,40 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
+from conans import ConanFile, tools, CMake
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    description = "Keep it short"
-    no_copy_source = True
+class VulkanHeadersConan(ConanFile):
+    name = "Vulkan-Headers"
+    version = "1.1.85.0"
+    url = "https://github.com/barcharcraz/conan-packages"
+    author = "Charles Barto <bartoc@umich.edu>"
+    description = "Headers for Vulkan"
+    no_copy_source = False
 
     # Indicates License type of the packaged library
-    license = "MIT"
+    license = "Apache 2.0"
 
     # Packages the license for the conanfile.py
-    exports = ["LICENSE.md"]
+    exports = ["LICENSE.txt"]
+
+    exports_sources = ["CMakeLists.txt"]
 
     # Custom attributes for Bincrafters recipe conventions
-    source_subfolder = "source_subfolder"
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+
+    generators = "cmake"
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = self.name + "-" + self.version
+        source_url = "https://github.com/KhronosGroup/Vulkan-Headers"
+        tools.get("{0}/archive/sdk-{1}.tar.gz".format(source_url, self.version),
+            sha256="d3c6435d101bb9271019c9d0dd159e210a805958dda3bb13344fce9316c6d098")
+        extracted_dir = "Vulkan-Headers-sdk-" + self.version
+        
 
         #Rename to "source_folder" is a convention to simplify later steps
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
+        tools.replace_in_file("source_subfolder/CMakeLists.txt",
+            r"${CMAKE_SOURCE_DIR}", r"${CMAKE_CURRENT_SOURCE_DIR}")
+
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
+    
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+    
 
     def package(self):
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
-        self.copy(pattern="*", dst="include", src=include_folder)
+        cmake = self._configure_cmake()
+        cmake.install()
 
 
     def package_id(self):
---
+++ Vulkan-Loader/conanfile.py
@@ -0,0 +1,91 @@
+from conans import ConanFile, CMake, tools
+import platform
+import os
+
+
+class VulkanLoaderConan(ConanFile):
+    name = "Vulkan-Loader"
+    version = "1.1.85.0"
+    url = "https://github.com/barcharcraz/conan-packages"
+    author = "Charlies Barto <bartoc@umich.edu>"
+    description = "Khronos official Vulkan ICD desktop loader for Windows, Linux, and MacOS."
+    license = "Apache 2.0"
+    
+    exports = ["LICENSE.txt"]
+    exports_sources = "CMakeLists.txt"
+
+    requires = "Vulkan-Headers/{}@bartoc/testing".format(version)
+
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+
+    generators = "cmake"
+
+    settings = "os", "arch", "build_type", "compiler"
+
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "with_wsi_xcb": [True, False],
+        "with_wsi_xlib": [True, False],
+        "with_wsi_wayland": [True, False],
+        "with_wsi_mir": [True, False],
+        "win10_onecore": [True, False],
+        "build_loader": [True, False],
+        "build_tests": [True, False]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": False,
+        "with_wsi_xcb": True,
+        "with_wsi_xlib": True,
+        "with_wsi_wayland": True,
+        "with_wsi_mir": False,
+        "win10_onecore": False,
+        "build_loader": True,
+        "build_tests": True
+    }
+    default_channel = "testing"
+    default_user = "bartoc"
+
+    def source(self):
+        source_url = "https://github.com/KhronosGroup/Vulkan-Loader"
+        tools.get("{0}/archive/sdk-{1}.tar.gz".format(source_url, self.version))
+        extracted_dir = self.name + "-sdk-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+    
+    def config_options(self):
+        if self.settings.os != "Windows":
+            del self.options.win10_onecore
+        if self.settings.os != "Linux":
+            del self.options.with_wsi_xcb
+            del self.options.with_wsi_xlib
+            del self.options.with_wsi_wayland
+            del self.options.with_wsi_mir
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        if self.settings.os == "Linux":
+            cmake.definitions["BUILD_WSI_XCB_SUPPORT"] = self.options.with_wsi_xcb
+            cmake.definitions["BUILD_WSI_XLIB_SUPPORT"] = self.options.with_wsi_xlib
+            cmake.definitions["BUILD_WSI_WAYLAND_SUPPORT"] = self.options.with_wsi_wayland
+            cmake.definitions["BUILD_WSI_MIR_SUPPORT"] = self.options.with_wsi_mir
+        if self.settings.os == "Windows":
+            cmake.definitions["ENABLE_WIN10_ONECORE"] = self.options.win10_onecore
+        cmake.definitions["ENABLE_STATIC_LOADER"] = not self.options.shared
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
+    
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+    
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+        
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+    
+    
+
