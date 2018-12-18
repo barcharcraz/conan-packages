bc7b29b197ac276f5abf4ebc7191a342eaba7436
Charles Barto <bartoc@umich.edu>
2018-12-18 14:38:31 -0500

    add almost working luxcore

--- LuxCore/conanfile.py
+++ LuxCore/conanfile.py
@@ -1,10 +1,10 @@
 from conans import ConanFile, CMake, tools
 import os
-
+import sys
 
 class LuxcoreConan(ConanFile):
     name = "LuxCore"
-    version = "2.1beta3"
+    version = "2.1beta4"
     license = "Apache 2.0"
     author = "Charles Barto <bartoc@umich.edu>"
     url = "https://github.com/barcharcraz/conan-packages"
@@ -15,21 +15,31 @@ class LuxcoreConan(ConanFile):
     default_options = "shared=False"
     generators = "cmake"
     requires = (
-        "boost/[>=1.56.0]@conan/stable"
+        "boost/[>=1.56.0]@conan/stable",
+        "OpenImageIO/2.0.3@bartoc/stable",
+        "openexr/2.3.0@conan/stable",
+        "embree/3.3.0@bartoc/stable",
+        "TBB/2019_U3@conan/stable",
+        "c-blosc/1.15.1@francescalted/stable"
     )
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
+    def configure(self):
+        self.options["boost"].without_python = False
+
     def source(self):
         source_url = "https://github.com/LuxCoreRender/LuxCore"
         tools.get("{0}/archive/luxcorerender_v{1}.tar.gz".format(source_url, self.version), 
-            sha256="4a6c9b0dea6b29a4ae758d8b120d8fd1175ec6fb5ad2a9cb1ed4306b1cc274e4")
+            sha256="ae7c20bb504b431e58dc82d2dfff9f819c6a9706115d2bbfc866a98eeacc1eb1")
         extracted_dir = self.name + "-" + "luxcorerender_v" + self.version
 
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
         cmake = CMake(self)
+        cmake.definitions["PYTHON_V"] = f"{sys.version_info.major}{sys.version_info.minor}"
+        cmake.definitions["LUXRAYS_DISABLE_OPENCL"] = True
         cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
--- embree/conanfile.py
+++ embree/conanfile.py
@@ -1,45 +1,62 @@
-
 from conans import ConanFile, CMake, tools
-import shutil
+
 import os
+import sys
 
 class EmbreeConan(ConanFile):
     name = "embree"
-    version = "2.16.4"
+    version = "3.3.0"
     license = "Apache 2.0"
     description = "The embree ray tracing library"
     url = "https://github.com/barcharcraz/conan-packages"
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "build_type", "compiler", "arch"
     generators = "cmake"
+    exports_sources = ["CMakeLists.txt"]
     options = {
-        "shared": [True, False]
+        "shared": [True, False],
+        "fPIC": [True, False]
     }
-    default_options = "shared=False"
-    def source(self):
-        zip_name = f"{self.name}-{self.version}.zip"
-        tools.download(f"https://github.com/embree/embree/archive/v{self.version}.zip", zip_name)
-        tools.unzip(zip_name)
-        shutil.move(f"{self.name}-{self.version}", f"{self.name}")
-        os.unlink(zip_name)
+    default_options = {
+        "shared": False,
+        "fPIC": True
+    }
+    default_channel = "testing"
+    default_user = "bartoc"
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
+    def source(self):
+        source_url = "https://github.com/embree/embree"
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version), 
+            sha256="b7afee01034544d80cce4f81eb3ead03c527728186ff96b4662a242252e224f6")
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+    
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["EMBREE_STATIC_RUNTIME"] = True if "MT" in self.settings.compiler.runtime else False
+        cmake.definitions["EMBREE_STATIC_LIB"] = not self.options.shared
+        cmake.definitions["EMBREE_TASKING_SYSTEM"] = "INTERNAL"
+        cmake.definitions["EMBREE_TUTORIALS"] = False
+        cmake.definitions["EMBREE_MAX_ISA"] = "SSE2"
+        cmake.definitions["EMBREE_ISPC_SUPPORT"] = False
+        cmake.definitions["EMBREE_TESTING_BENCHMARK"] = False
+        cmake.definitions["EMBREE_TESTING_SDE"] = False
+        cmake.definitions["EMBREE_TESTING_PACKAGE"] = False
+        cmake.definitions["EMBREE_STACK_PROTECTOR"] = False
+        cmake.definitions["EMBREE_RAY_PACKETS"] = False
+        cmake.definitions["EMBREE_RAY_MASK"] = False
+        
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
+    
     def build(self):
-        cmake = CMake(self.settings)
-        cmake_opts = [
-            f"-DCMAKE_INSTALL_PREFIX={self.package_folder}",
-            f"-DBUILD_SHARED_LIBS={self.options.shared}",
-            f"-DEMBREE_STATIC_LIB={not self.options.shared}",
-            "-DEMBREE_TUTORIALS=OFF",
-            "-DEMBREE_TASKING_SYSTEM=INTERNAL",
-            "-DEMBREE_ISPC_SUPPORT=OFF",
-            "-DEMBREE_MAX_ISA=SSE2",
-            "-DEMBREE_GEOMETRY_SUBDIV=OFF",
-        ]
-        if self.settings.os == "Windows":
-            cmake_opts.append("-DEMBREE_STATIC_RUNTIME=ON" if "MT" in str(self.settings.compiler.runtime) else "-DEMBREE_STATIC_RUNTIME=OFF")
-        self.run(f"cmake {self.name} {' '.join(cmake_opts)} {cmake.command_line}")
-        self.run(f"cmake --build . {cmake.build_config}")
+        cmake = self._configure_cmake()
+        cmake.build()
     
     def package(self):
-        cmake = CMake(self.settings)
-        self.run(f"cmake --build . --target install {cmake.build_config}")
-        self.copy("*.lib", src=".", dst="lib", keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
+    
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
