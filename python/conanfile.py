e46bdabadc8111b34d992274a33c87b2f5e2b5ed
charlie barto <bartoc@umich.edu>
2018-12-20 10:55:07 -0500

    fix sdk version

--- python/conanfile.py
+++ python/conanfile.py
@@ -26,12 +26,19 @@ class PythonConan(ConanFile):
         "fPIC": True
     }
 
+    scm = {
+        "type": "git",
+        "subfolder": _source_subfolder,
+        "url": "https://github.com/python/cpython.git",
+        "revision": f"v{version}"
+    }
+
     def source(self):
-        source_url = f"https://www.python.org/ftp/python/{self.version}/Python-{self.version}.tgz"
-        tools.get(
-            source_url, sha256="36c1b81ac29d0f8341f727ef40864d99d8206897be96be73dc34d4739c9c9f06")
-        extracted_dir = f"Python-{self.version}"
-        os.rename(extracted_dir, self._source_subfolder)
+        # source_url = f"https://www.python.org/ftp/python/{self.version}/Python-{self.version}.tgz"
+        # tools.get(
+        #     source_url, sha256="36c1b81ac29d0f8341f727ef40864d99d8206897be96be73dc34d4739c9c9f06")
+        # extracted_dir = f"Python-{self.version}"
+        # os.rename(extracted_dir, self._source_subfolder)
 
         
         tools.patch(base_path=self._source_subfolder, patch_file="0005-Fix-DefaultWindowsSDKVersion.patch")
